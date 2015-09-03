13aaee177550546e9d9c31f1a4e92eab7fcddd75
Christer V. Aas <christer@sercon.no>
2015-09-03 21:36:43 +0200

    Added new script.

---
+++ PowershellServerInstall/AppServerRabbitMQIISCouchbase.ps1
@@ -0,0 +1,11 @@
+<#
+.SYNOPSIS
+   <A brief description of the script>
+.DESCRIPTION
+   <A detailed description of the script>
+.PARAMETER <paramName>
+   <Description of script parameter>
+.EXAMPLE
+   <An example of using the script>
+#>
+
