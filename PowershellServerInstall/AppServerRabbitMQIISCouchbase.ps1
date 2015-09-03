87f11b9c065608098bc8e0256091073d5db92c9a
Christer V. Aas <christer@sercon.no>
2015-09-03 21:46:28 +0200

    New script to install Erlang and RabbitMQ from sites.

--- PowershellServerInstall/AppServerRabbitMQIISCouchbase.ps1
+++ PowershellServerInstall/AppServerRabbitMQIISCouchbase.ps1
@@ -9,3 +9,33 @@
    <An example of using the script>
 #>
 
+$webclient = New-Object System.Net.WebClient
+
+md "C:\tmpInstall"
+$urlErlang = "http://www.erlang.org/download/otp_win64_18.0.exe"
+$urlRabbit = "https://www.rabbitmq.com/releases/rabbitmq-server/v3.5.4/rabbitmq-server-3.5.4.exe"
+
+$fileRabbit = "C:\tmpInstall\rabbitmq-server-3.5.4.exe"
+$fileErlang = "C:\tmpInstall\otp_win64_18.0.exe"
+
+If (Test-Path $fileErlang){
+  Write-Output "Erlang 18.0 already downloaded."
+}Else{
+  Write-Output "Erlang 18.0 download started."
+  $webclient.DownloadFile($urlRabbit,$fileRabbit)
+}
+
+If (Test-Path $fileRabbit){
+  Write-Output "RabbitMQ 3.5.4 already downloaded."
+}Else{
+  Write-Output "RabbitMQ 3.5.4 download started."
+  $webclient.DownloadFile($urlErlang,$fileErlang)
+}
+
+Start-Process $fileErlang -ArgumentList "/S" -Wait
+Write-Output "Erlang 18.0 installed."
+Start-Process $fileRabbit -ArgumentList "/S" -Wait
+Write-Output "RabbitMQ 3.5.4 installed."
+
+#if ($setup.exitcode -eq 0)
+#write-host "Successfully installed" 
